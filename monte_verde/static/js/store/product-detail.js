386d09e18634dd0d315469efa6949e16bdcaa5c7
nelson <nelsonrca2006@gmail.com>
2025-07-15 23:24:56 -0500

    funcionalidad product-detail

--- monte_verde/static/js/store/product-detail.js
+++ monte_verde/static/js/store/product-detail.js
@@ -33,6 +33,7 @@ button.addEventListener('click', () => {
 
     addProductToShoppingCart(name, price);
     showProductAddedNotification();
+    updateCountShoppingCart();
 })
 
 
@@ -44,4 +45,14 @@ function showProductAddedNotification(){
     setTimeout(() => {
         notification.classList.remove('show');
     }, 1000);
+}
+
+// función para actualizar cantidad de productos en el ícono del carrito
+function updateCountShoppingCart(){
+    const span = document.querySelectorAll('.shopping-cart--count');
+    const count = JSON.parse(localStorage.getItem('shopping_cart')).length;
+
+    span.forEach(s => {
+        s.textContent = count;
+    })
 }
