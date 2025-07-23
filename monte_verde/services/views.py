71c1742b612d1c0f4ce03349c533c7ae0247dfe6
nelson <nelsonrca2006@gmail.com>
2025-07-23 18:00:41 -0500

    added funcionality

--- monte_verde/services/views.py
+++ monte_verde/services/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.conf import settings
 from django.core.mail import send_mail
+from django.http import JsonResponse
 
 from services.models import PhoneNumber
 
 def services_view(request):
     number = PhoneNumber.objects.all().order_by('-id').first()
 
-    if request.method == 'POST':
+    if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         service = request.POST.get('service')
@@ -17,14 +18,12 @@ def services_view(request):
         subject = f'Quiero el servicio: {service}'
         message_body = f'Nombre: {name}\nNúmero: {phone}\n\n{message}'
         from_email = settings.DEFAULT_FROM_EMAIL 
-        recipient_list = ['ericka@viveromonteverde.com']
+        recipient_list = ['nelsonrca2006@gmail.com']
 
         try:
             send_mail(subject, message_body, from_email, recipient_list)
-            messages.success(request, '¡El mensaje se ha enviado correctamente!')
+            return JsonResponse({'success': True})
         except Exception as e:
-            messages.error(request, 'Hubo un error al enviar el mensaje. Por favor, intenta de nuevo.')
-
-        return redirect('services')
+            return JsonResponse({'success': False, 'error': str(e)}, status=500)
 
     return render(request, 'services.html', { 'number': number })
