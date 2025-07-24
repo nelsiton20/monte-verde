bb5d3f43cc94c1f48947f3482855510df119380c
nelson <nelsonrca2006@gmail.com>
2025-07-24 07:09:48 -0500

    funcionalidad terminada

--- monte_verde/featured_products/models.py
+++ monte_verde/featured_products/models.py
@@ -5,7 +5,8 @@ from store.models import Product
 # Create your models here.
 class FeaturedProduct(models.Model):
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE, unique=True)
-    content = models.CharField(max_length=70, null=False, blank=False)
+    title = models.CharField(max_length=30, null=False, blank=False)
+    content = models.CharField(max_length=75, null=False, blank=False)
 
     def __str__(self):
         return self.product.name
